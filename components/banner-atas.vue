627b196dd8ba32a27c943abb427155e9d02056be
Akhyar <ayankcell@gmail.com>
2023-11-01 09:26:31 +0700

    feat: animate-ping

--- components/banner-atas.vue
+++ components/banner-atas.vue
@@ -2,7 +2,7 @@
     <!-- banner 10.10 -->
     <div class="w-full h-12 bg-red-600 flex items-center text-white text-sm justify-center" v-show="!terlewat">
         <div class="font-bold px-2">
-            Sadisnya <span class="inline-block text-lg animate-pulse">Promo 11.11</span>
+            Sadisnya <span class="inline-block text-lg animate-ping">Promo 11.11</span>
         </div>
         <div v-show="dayjs().isBefore(dDay,'day')">{{ dayjs(dDay).fromNow() }}</div>
         <div v-show="dayjs().isSame(dDay,'day')" class="font-semibold">HARI INI</div>
