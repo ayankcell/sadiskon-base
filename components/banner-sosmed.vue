7e2c7ea68a23d2c2edb7e23b2856d79d6ad1522f
ayankcell <ayankcell@gmail.com>
2024-03-13 13:02:13 +0700

    chore: update label

--- components/banner-sosmed.vue
+++ components/banner-sosmed.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full px-2 py-3 flex justify-center items-center space-x-1 bg-gradient-to-br from-gray-600 to-slate-400 text-white text-sm rounded">
         <div>
-            Gabung <span class="font-semibold">Komunitas Hemat</span> di Sosial Media
+            Gabung <span class="font-semibold">Komunitas</span> di WhatsApp & Sosial Media
         </div>
         <UButton type="link" to="https://www.sadiskon.com/biolink/" color="orange" size="xs">Ayo!</UButton>
     </div>
