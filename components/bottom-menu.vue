0d36c72f8a73dc17dc6262515f1f543bd0676e81
ayankcell <ayankcell@gmail.com>
2024-09-09 09:14:28 +0700

    update menu

---
+++ components/bottom-menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="fixed bottom-0 left-0 w-full z-20">
+        <div class="max-w-lg mx-auto bg-white flex text-gray-700 shadow-md">
+            <ULink active-class="text-red-700" :to="menu.to"
+                class="py-1 flex flex-1 flex-col justify-center items-center" :class="menu.animation ?? ''"
+                v-for="menu of data?.menuFooter?.links" :key="menu.label"
+                @click="menu.action ? (menuIsOpen = !menuIsOpen) : ''" external>
+                <NuxtImg :src="`${jsonHost}/icons/${menu.icon}`" class="w-5 h-5 opacity-70" />
+                <span class="text-xs">{{ menu.label }}</span>
+            </ULink>
+        </div>
+    </div>
+</template>
+<script setup>
+const { menuIsOpen } = useMenuSlide()
+const jsonHost = 'https://sds-jsons.pages.dev'
+
+const { data } = await useFetch(`${jsonHost}/menu-footer-v2.json`)</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,39 +10,7 @@
       <Footer />
       <!-- buttom navigation on root path -->
       <div class="block w-full h-14"></div>
-      <div class="fixed bottom-0 left-0 w-full z-20">
-        <div class="max-w-lg mx-auto bg-white flex text-gray-700 shadow-md">
-          <ULink active-class="text-red-700" :to="menu.to" class="py-1 flex flex-1 flex-col justify-center items-center"
-            :class="menu.animation ?? ''" v-for="menu of data?.menuFooter?.links" :key="menu.label"
-            @click="menu.action ? (menuIsOpen = !menuIsOpen) : '' ">
-            <UIcon :name="menu.icon" />
-            <span class="text-xs">{{ menu.label }}</span>
-          </ULink>
-        </div>
-      </div>
+     <BottomMenu/>
     </UContainer>
   </div>
 </template>
-
-<script setup lang="ts">
-const { menuIsOpen } = useMenuSlide()
-
-interface FooterLink {
-    label: string;
-    icon: string;
-    to: string;
-    animation: boolean;
-    action: boolean;
-  }
-  
-  interface MenuFooter {
-    lastUpdated: string;
-    links: FooterLink[];
-  }
-  
-  interface ApiResponse {
-    menuFooter: MenuFooter;
-  }
-const { data } = await useFetch<ApiResponse>('https://sds-jsons.pages.dev/menu-footer.json')
-
-</script>
